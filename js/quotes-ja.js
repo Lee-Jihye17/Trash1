fac95f5ab0f3f4f43aee51d1ca307fa98a48910d
Lee-Jihye <taiyoaiyu@taiyonoMacBook-Air.local>
2024-11-12 13:47:04 +0900

    tada

--- js/quotes-ja.js
+++ js/quotes-ja.js
@@ -1,19 +1,18 @@
 var quotes = [
     {
-        "author": "ゲイ",
+        "author": "G.A.Y",
         "quote": "「私は朝鏡を見れない。朝から息子で鏡を破るわけにはいかないから。」"
     },
     {
-        "author": "三上まおと",
+        "author": "M.M",
         "quote": "SEX!SEXSEXSEXSEXSEXSEXSEX!!!!!"
     },
     {
-        "author": "田原果門",
+        "author": "K.T",
         "quote": "私の体は女を犯すため、私の右手は女を殴るため、私の左手は女を絞めるために存在している。"
     },
     {
-        "author": "白木俊介",
+        "author": "S.S",
         "quote": "こんにthiは、ぼくはthiらきthixゅんthiけだよ(イケボ)"
     },
-    // ... 既存のコードは続く ...
 ] 
